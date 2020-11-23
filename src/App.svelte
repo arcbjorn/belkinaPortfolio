8a65b8aba8a365a477d25fec3ee4f6dacf29bdd1
arcbjorn <arky.happ@gmail.com>
2020-11-23 22:30:25 +0300

    ci (global): test ci with netlify

--- src/App.svelte
+++ src/App.svelte
@@ -5,22 +5,28 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>{greeting}</p>
+	<h3>{greeting}</h3>
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		padding: 4em;
+		max-width: 600px;
 		margin: 0 auto;
 	}
 
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
+		font-size: 8em;
+		font-weight: 800;
+	}
+
+  h3 {
+		color: #ff3e00;
 		font-size: 4em;
-		font-weight: 100;
+		font-weight: 600;
 	}
 
 	@media (min-width: 640px) {
